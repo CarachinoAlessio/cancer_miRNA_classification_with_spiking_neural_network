1a89bbc11d05522036bb2d375419a515c0e615c9
Alessio Carachino <alessio.carachino.g@gmail.com>
2023-09-20 18:23:17 +0200

    implemented feature selection filter ...on dummy data

--- src/utils/feature_selection.py
+++ src/utils/feature_selection.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import os
 
@@ -6,10 +7,11 @@ class FeatureSelection():
     def __init__(
             self,
             feature_path: str,
-            n_superclasses: int
+            superclasses: list[list]
         ) -> None:
         self.features = self.__load_features(feature_path=feature_path)
-        self.n_superclasses = n_superclasses
+        self.superclasses = superclasses
+        self.n_superclasses = len(self.superclasses)
 
     def __load_features(self, feature_path: str) -> pd.DataFrame:
         '''
@@ -24,6 +26,46 @@ class FeatureSelection():
         assert os.path.isfile(feature_path)
         return pd.read_csv(feature_path)
     
-    def reduce_dimensionality(self, data, method='fisher', train=True) -> pd.DataFrame:
-        # TODO
+    def reduce_dimensionality(self, data: list[list], inference_mode=False) -> pd.DataFrame:
+
+        result = []
+        data = np.asarray(data)
+        for i, superclass in enumerate(self.superclasses):
+            indici = [j for j, d in enumerate(data) if d[-1] in superclass]
+            subset = data[indici]
+
+            indici_features = self.features[f"Super-class{i}"].tolist()
+            subset = np.asarray(subset).T
+
+            subset = subset[indici_features]
+            result.append(subset.T)
+            print('hell')
+
+
+        return result
         pass
+
+    def find_feature_subset(self, data, method='fisher', n_features=300):
+        pass
+
+
+data = [
+            [1,2,4,5,'easp'],
+            [3,4,4,5,'fppp'],
+            [9,9,4,5,'ciao']
+        ]
+
+superclasses = [
+            ['easp'],
+            ['fppp', 'ciao']
+        ]
+
+output = [
+    [[1,2,4,5,'easp']],
+    [[3,4,4,5,'fppp'], [9,9,4,5,'ciao']]
+]
+
+
+fs = FeatureSelection('data/features/dummy_features.csv', superclasses)
+output = fs.reduce_dimensionality(data)
+print('stop')
