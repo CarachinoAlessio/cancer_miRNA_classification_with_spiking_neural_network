3037b19fb0d77c94f116cf39352120524e569358
Alessio Carachino <alessio.carachino.g@gmail.com>
2023-09-21 17:13:19 +0200

    fix dtype

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 def data_split_to_superclasses(data, superclasses):
     '''
     common function to split original data into supersets according to the superclasses
@@ -11,5 +12,5 @@ def data_split_to_superclasses(data, superclasses):
         indici = [j for j, d in enumerate(data) if d[-1] in superclass]
         subset = data[indici]
         labels.append([s[-1] for s in subset])
-        subsets.append([s[:-1] for s in subset])
+        subsets.append([np.asarray(s[:-1], dtype=float) for s in subset])
     return subsets, labels
