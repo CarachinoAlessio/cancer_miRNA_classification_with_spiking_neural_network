29f9dc864db51ab576a2f77f7b1ea4cf2b7033e3
Alessio Carachino <alessio.carachino.g@gmail.com>
2023-09-21 13:08:16 +0200

    started implementation of fisher discriminant ratio + util function and added a package dependence

--- src/utils/feature_selection.py
+++ src/utils/feature_selection.py
@@ -1,14 +1,18 @@
+from typing import Tuple, Any, List
+
+from skfeature.function.similarity_based import fisher_score
+from utils import data_split_to_superclasses
 import numpy as np
 import pandas as pd
 import os
 
 
-class FeatureSelection():
+class FeatureSelection:
     def __init__(
             self,
             feature_path: str,
             superclasses: list[list]
-        ) -> None:
+    ) -> None:
         self.features = self.__load_features(feature_path=feature_path)
         self.superclasses = superclasses
         self.n_superclasses = len(self.superclasses)
@@ -25,47 +29,74 @@ class FeatureSelection():
         '''
         assert os.path.isfile(feature_path)
         return pd.read_csv(feature_path)
-    
-    def reduce_dimensionality(self, data: list[list], inference_mode=False) -> pd.DataFrame:
 
-        result = []
+    def reduce_dimensionality(self, data: list[list], inference_mode: bool = False) -> tuple[list[Any], list[Any]]:
+        '''
+        :param data: list containing samples (data + label)
+        :param inference_mode: not used... yet
+        :return: reduced data according to their superclass feature subset selection
+        '''
+        reduced_data = []
+        labels = []
         data = np.asarray(data)
-        for i, superclass in enumerate(self.superclasses):
-            indici = [j for j, d in enumerate(data) if d[-1] in superclass]
-            subset = data[indici]
+        subsets, labels = data_split_to_superclasses(data, self.superclasses)
 
+        for i, superclass in enumerate(self.superclasses):
+            subset = subsets[i]
             indici_features = self.features[f"Super-class{i}"].tolist()
             subset = np.asarray(subset).T
 
             subset = subset[indici_features]
-            result.append(subset.T)
-            print('hell')
+            reduced_data.append(subset.T)
+
+        return reduced_data, labels
+
+    def find_features_subset(self, data, method='fisher', n_features=300, export_to_csv=False):
+        subsets, super_labels = data_split_to_superclasses(data, self.superclasses)
+        selected_features = []
+        if method == 'fisher':
+            for i, superclass in enumerate(superclasses):
+                data = subsets[i]
+                labels = super_labels[i]
+                idx = fisher_score.fisher_score(data, labels, mode='rank')
+                num_features = n_features
+                idx = idx[0:num_features]
+                selected_features.append(idx)
+                print(idx)
+                selected_features_train = data[:, idx]
+                if export_to_csv:
+                    # todo
+                    print('todo')
+                return selected_features
+        else:
+            raise Exception(f'{method} not implemented... yet.')
 
 
-        return result
-        pass
+'''
+Testing module with dummy data below
+'''
 
-    def find_feature_subset(self, data, method='fisher', n_features=300):
-        pass
-
-
-data = [
-            [1,2,4,5,'easp'],
-            [3,4,4,5,'fppp'],
-            [9,9,4,5,'ciao']
-        ]
+data = np.asarray([
+    [1, 2, 4, 5, 'easp'],
+    [10, 20, 40, 50, 'prova'],
+    [3, 4, 4, 5, 'fppp'],
+    [9, 9, 4, 5, 'ciao']
+])
 
 superclasses = [
-            ['easp'],
-            ['fppp', 'ciao']
-        ]
-
-output = [
-    [[1,2,4,5,'easp']],
-    [[3,4,4,5,'fppp'], [9,9,4,5,'ciao']]
+    ['easp', 'prova'],
+    ['fppp', 'ciao']
 ]
 
+output = [
+    [[1, 2, 4, 5, 'easp'], [10, 20, 40, 50, 'prova']],
+    [[3, 4, 4, 5, 'fppp'], [9, 9, 4, 5, 'ciao']]
+]
 
 fs = FeatureSelection('data/features/dummy_features.csv', superclasses)
-output = fs.reduce_dimensionality(data)
+
+#selected_features_idx = fs.find_features_subset(data, method='fisher', n_features=2, export_to_csv=True)
+
+reduced_data, labels = fs.reduce_dimensionality(data)
+
 print('stop')
---
+++ src/utils/utils.py
@@ -0,0 +1,15 @@
+def data_split_to_superclasses(data, superclasses):
+    '''
+    common function to split original data into supersets according to the superclasses
+    :param data: containing samples (data + label)
+    :param superclasses:
+    :return:
+    '''
+    labels = []
+    subsets = []
+    for i, superclass in enumerate(superclasses):
+        indici = [j for j, d in enumerate(data) if d[-1] in superclass]
+        subset = data[indici]
+        labels.append([s[-1] for s in subset])
+        subsets.append([s[:-1] for s in subset])
+    return subsets, labels
