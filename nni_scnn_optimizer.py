eaf41fec1e51f949bc8fc28b41b02591f28da27c
root <francesco.sorrentino269@gmail.com>
2023-10-26 12:35:09 +0200

    nni for SCNN (further tests needed)
    Co-authored-by: Francesco Di Gangi <FDG2801@users.noreply.github.com>

---
+++ nni_scnn_optimizer.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import snntorch as snn
+from snntorch import backprop
+from snntorch import functional as SF
+from snntorch import utils
+from snntorch import spikeplot as splt
+import nni
+import argparse
+#SCNN lib
+from src.models.SCNN import SCNN
+from src.utils.dataloader import load_dataset
+
+device = ("cuda" if torch.cuda.is_available() else "cpu")
+
+superclasses = [
+    ['BRCA', 'KICH', 'KIRC', 'LUAD', 'LUSC', 'MESO', 'SARC', 'UCEC'],
+    ['BLCA', 'CESC', 'HNSC', 'KIRP', 'PAAD', 'READ', 'STAD'],
+    ['DLBC', 'LGG', 'PRAD', 'TGCT', 'THYM', 'UCS'],
+    ['ACC', 'CHOL', 'LIHC'],
+    ['ESCA', 'PCPG', 'SKCM', 'THCA', 'UVM']
+]
+
+
+def train(dataloader, model, loss_fn, optimizer, num_steps):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+        spk_rec, _ = model(num_steps, X)
+        loss_val = loss_fn(spk_rec, y)
+        optimizer.zero_grad()
+        loss_val.backward()
+        optimizer.step()
+
+
+def test(dataloader, model, loss_fn, num_steps):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    total = 0
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            spk_rec, _ = model(num_steps, X)
+            correct += SF.accuracy_rate(spk_rec, y) * spk_rec.size(1)
+            total += spk_rec.size(1)
+    correct /= total
+    return correct
+
+
+
+def main_scnn_optimization(params, metalabel, labels_of_metaclass):
+    num_classes = len(labels_of_metaclass)
+
+    print(f'METACLASS LABELS: {labels_of_metaclass}')
+    train_dataloader, test_dataloader = load_dataset(name='cancer', batch_size=params['batch_size'],
+                                                     metalabel=metalabel, labels_of_metaclass=labels_of_metaclass)
+
+    filter_numbers = [params['nf1'], params['nf2'], params['nf3']]
+    convolution_windows = [params['cw1'], params['cw2'], params['cw3']]
+    max_pooling_windows = [params['pw1'], params['pw2'], params['pw3']]
+    final_nf = params['nf4']
+    beta = params['beta']
+    num_step = params['num_step']
+
+    model = SCNN(num_classes, filter_numbers, convolution_windows, max_pooling_windows, final_nf, beta).to(device)
+    epochs = 5  # TODO: be careful
+    loss_fn = SF.ce_rate_loss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=params['lr'])
+
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, num_steps=num_step)
+        accuracy = test(test_dataloader, model, loss_fn, num_steps=num_step)
+        print(accuracy)
+        nni.report_intermediate_result(accuracy)
+    nni.report_final_result(accuracy)
+    # input('Premi un tasto per concludere l esperimento...')
+
+
+def get_params():
+    ''' Get parameters from command line '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nf1", type=int, default=2)
+    parser.add_argument("--nf2", type=int, default=2)
+    parser.add_argument("--nf3", type=int, default=2)
+    parser.add_argument("--nf4", type=int, default=2)
+    parser.add_argument("--cw1", type=int, default=4)
+    parser.add_argument("--cw2", type=int, default=4)
+    parser.add_argument("--cw3", type=int, default=4)
+    parser.add_argument("--pw1", type=int, default=4)
+    parser.add_argument("--pw2", type=int, default=4)
+    parser.add_argument("--pw3", type=int, default=4)
+    parser.add_argument("--beta", type=float, default=0.8)
+    parser.add_argument("--num_step", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--lr", type=float, default=1e-2)
+
+    parser.add_argument("--superclass", type=int, help='Please specify the superclass you want to run the experiment '
+                                                       'for.')
+    args, _ = parser.parse_known_args()
+    return args
+
+
+if __name__ == '__main__':
+    superclass_target = 0
+    metaclass_labels = superclasses[superclass_target]
+    print(f'RUNNING EXPERIMENT FOR SUPERCLASS {superclass_target}')
+
+    try:
+        # get parameters form tuner
+        tuner_params = nni.get_next_parameter()
+        # logger.debug(tuner_params)
+        params = vars(get_params())
+        params.update(tuner_params)
+        main_scnn_optimization(params, superclass_target, metaclass_labels)
+    except Exception as exception:
+        # logger.exception(exception)
+        raise
